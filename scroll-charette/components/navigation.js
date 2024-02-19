fae13425feb7e11804cc9e25fd8b43ad2b07af19
Chelsea Yuan <70819287+chelsealyuan@users.noreply.github.com>
2024-02-19 09:36:41 -0600

    finished first 4 charettes

--- scroll-charette/components/navigation.js
+++ scroll-charette/components/navigation.js
@@ -5,9 +5,6 @@ class Header extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-        <style>
-          
-        </style>
         <header>
           <nav>
             <a href="index.html">Home</a>
@@ -23,19 +20,19 @@ class Header extends HTMLElement {
             <a href="page10.html">Page 10</a>
             <a href="page11.html">Page 11</a>
             <a href="page12.html">Page 12</a>
-            <a href="/pages/page13.html">Page 13</a>
-            <a href="/pages/page14.html">Page 14</a>
-            <a href="/pages/page15.html">Page 15</a>
-            <a href="/pages/page16.html">Page 16</a>
-            <a href="/pages/page17.html">Page 17</a>
-            <a href="/pages/page18.html">Page 18</a>
-            <a href="/pages/page19.html">Page 19</a>
-            <a href="/pages/page20.html">Page 20</a>
-            <a href="/pages/page21.html">Page 21</a>
-            <a href="/pages/page22.html">Page 22</a>
-            <a href="/pages/page23.html">Page 23</a>
-            <a href="/pages/page24.html">Page 24</a>
-            <a href="/pages/page25.html">Page 25</a>
+            <a href="page13.html">Page 13</a>
+            <a href="page14.html">Page 14</a>
+            <a href="page15.html">Page 15</a>
+            <a href="page16.html">Page 16</a>
+            <a href="page17.html">Page 17</a>
+            <a href="page18.html">Page 18</a>
+            <a href="page19.html">Page 19</a>
+            <a href="page20.html">Page 20</a>
+            <a href="page21.html">Page 21</a>
+            <a href="page22.html">Page 22</a>
+            <a href="page23.html">Page 23</a>
+            <a href="page24.html">Page 24</a>
+            <a href="page25.html">Page 25</a>
           </nav>
         </header>
       `;
