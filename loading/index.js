cc3476f04919805fde1137f85b49c583a54bdd3a
Chelsea Yuan <70819287+chelsealyuan@users.noreply.github.com>
2024-04-07 22:31:14 -0500

    uploading ideas and sketches

---
+++ loading/index.js
@@ -0,0 +1,39 @@
+let paintingData = [
+  {
+    category: "green",
+    name: "Dancer in Green",
+    artist: "Edgar Degas",
+    date: 1883,
+    colorPalette: [
+      "CFDAD8",
+      "ACAEA3",
+      "A0A79C",
+      "C2C4B5",
+      "9A8650",
+      "B5BEB8",
+      "74653D",
+    ],
+  },
+  {
+    category: "green",
+    name: "The Singer in Green",
+    artist: "Edgar Degas",
+    date: 1884,
+    colorPalette: [
+      "7A9471",
+      "535C51",
+      "888D50",
+      "616252",
+      "91843F",
+      "39423F",
+      "1E4955",
+    ],
+  },
+  {
+    category: "green",
+    name: "Roses",
+    artist: "Vincent Van Gogh",
+    date: 1890,
+    colorPalette: ["42825D", "77B392", "78956E", "698254", "CAC3A7"],
+  },
+];
