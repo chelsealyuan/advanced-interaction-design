2366dab076053dd85659e315a9a73e2590a1e4e3
Chelsea Yuan <70819287+chelsealyuan@users.noreply.github.com>
2024-04-08 09:07:29 -0500

    prototype 1

--- loading/index.js
+++ loading/index.js
@@ -4,6 +4,7 @@ let paintingData = [
     name: "Dancer in Green",
     artist: "Edgar Degas",
     date: 1883,
+    path: "idea2/green1.jpeg",
     colorPalette: [
       "CFDAD8",
       "ACAEA3",
@@ -19,6 +20,7 @@ let paintingData = [
     name: "The Singer in Green",
     artist: "Edgar Degas",
     date: 1884,
+    path: "idea2/green2.jpeg",
     colorPalette: [
       "7A9471",
       "535C51",
@@ -34,6 +36,68 @@ let paintingData = [
     name: "Roses",
     artist: "Vincent Van Gogh",
     date: 1890,
-    colorPalette: ["42825D", "77B392", "78956E", "698254", "CAC3A7"],
+    path: "idea2/green3.jpeg",
+    colorPalette: [
+      "77947D",
+      "A0AA98",
+      "B3AD9C",
+      "538560",
+      "C1BFB2",
+      "91A87E",
+      "78956E",
+    ],
   },
 ];
+
+// Generate new painting button
+let greenButton = document.querySelector(".get-painting-btn");
+greenButton.addEventListener("click", getPainting);
+
+// Function to randomly select an object from the paintingData array and return it
+function selectRandomPainting() {
+  // Randomly select an index from the paintingData array
+  let randomIndex = Math.floor(Math.random() * paintingData.length);
+
+  // Retrieve the selected painting object
+  let selectedPainting = paintingData[randomIndex];
+
+  // Return the selected painting object
+  return selectedPainting;
+}
+
+const paintingImage = document.querySelector(".painting");
+const colorOverlay = document.querySelector(".color-overlay");
+
+function getPainting() {
+  paintingImage.style.opacity = 0;
+  let painting = selectRandomPainting();
+
+  paintingImage.src = painting.path;
+
+  fillColors(painting);
+}
+
+function fillColors(painting) {
+  let imageWidth = painting.width;
+
+  for (let i = 1; i < 8; i++) {
+    setTimeout(function () {
+      let colorContainer = document.querySelector(".color" + i);
+      colorContainer.style.width = imageWidth + "px";
+      colorContainer.style.backgroundColor = "#" + painting.colorPalette[i - 1];
+    }, 200 * i);
+  }
+
+  // Load the image after colors are displayed
+  setTimeout(function () {
+    paintingImage.style.opacity = 1;
+  }, 200 * 8); // Adjust the delay as needed based on the number of colors displayed
+
+  // Hide the colors after the image is loaded
+  for (let i = 1; i < 8; i++) {
+    setTimeout(function () {
+      let colorContainer = document.querySelector(".color" + i);
+      colorContainer.style.backgroundColor = "transparent";
+    }, 200 * (8 + i)); // Increment the delay for each color
+  }
+}
